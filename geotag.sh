23e66415fdf64856aab63c9b0aab0296967a1ac0
William <throughnothing@gmail.com>
2010-10-02 22:07:35 -0400

    added timeoffset to geotag.sh

--- geotag.sh
+++ geotag.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 # $1 = directory to .gpx file + photos
+# $2 = seconds time offset where camera time + seconds = GMT
+#		(defaults to 14400 which will adjust for EST during daylight savings)
 
 if [[ ! -n "$1" ]]; then
 	echo "Usage: $0 [directory]"
 	exit 0;
 fi
 
-gpsPhoto.pl --gpsdir $1 --dir $1 --maxtimediff 60 --timeoffset 14400
+TIMEOFFSET=$2
+if [[ ! -n "$2" ]]; then
+	TIMEOFFSET=14400
+fi
+
+gpsPhoto.pl --gpsdir $1 --dir $1 --maxtimediff 60 --timeoffset $TIMEOFFSET
 
