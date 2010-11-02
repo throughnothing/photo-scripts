4598ef750581d74e8dd6a56ca69d84fa5b89665a
William <throughnothing@gmail.com>
2010-11-02 16:27:20 -0400

    separate gpx dir and photo dir

--- geotag.sh
+++ geotag.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# $1 = directory to .gpx file + photos
-# $2 = seconds time offset where camera time + seconds = GMT
+# $1 = directory to .gpx file(s)
+# $2 = directory of photos
+# $3 = seconds time offset where camera time + seconds = GMT
 #		(defaults to 14400 which will adjust for EST during daylight savings)
 
 if [[ ! -n "$1" ]]; then
-	echo "Usage: $0 [directory]"
+	echo "Usage: $0 [.gpx directory] [photo directory]"
+	exit 0;
+fi
+
+if [[ ! -n "$2" ]]; then
+	echo "Usage: $0 [.gpx directory] [photo directory]"
 	exit 0;
 fi
 
@@ -15,5 +21,5 @@ if [[ ! -n "$2" ]]; then
 	TIMEOFFSET=14400
 fi
 
-gpsPhoto.pl --gpsdir $1 --dir $1 --maxtimediff 600 --timeoffset $TIMEOFFSET
+gpsPhoto.pl --gpsdir $1 --dir $2 --maxtimediff 900 --timeoffset $TIMEOFFSET
 
