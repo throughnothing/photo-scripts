31ff07c273f6cab1768d550f75ba554ba63fe01e
William <throughnothing@gmail.com>
2010-11-27 13:00:37 -0500

    make geotag script also create the .kml files in the appropriate location

--- geotag.py
+++ geotag.py
@@ -2,6 +2,7 @@
 import sys, os, commands
 
 BASE_PHOTO_DIR="/home/throughnothing/Pictures/Photos"
+BASE_KML_DIR="/home/throughnothing/projects/throughnothing.com/travel/kml"
 
 def usage():
   print "%s [.gpx directory]" % (sys.argv[0])
@@ -18,15 +19,24 @@ for file in files:
   month = file[4:6]
   day   = file[6:8]
 
-  gps_file = "/".join([gps_dir,file]).replace(' ','\\ ').replace('-','\\-')
+  gps_file = "/".join([gps_dir,file])
 
-  photo_dir = os.path.abspath("/".join([BASE_PHOTO_DIR ,year,month,day]))
+  photo_dir = "/".join([BASE_PHOTO_DIR ,year,month,day])
   
   result = commands.getstatusoutput(
-    'gpsPhoto.pl --gpsfile %s --dir %s --maxtimediff 900 --timeoffset 0'
+    'gpsPhoto.pl --gpsfile "%s" --dir "%s" --maxtimediff 900 --timeoffset 0'
     % ( gps_file, photo_dir )
   )
   print result[1].split('\n')[-1]
+
+  #Convert .gpx to .kml and put in right place
+  kml_file = "/".join([BASE_KML_DIR,'-'.join([year,month,day])]) + '.kml'
+  result = commands.getstatusoutput(
+    './external/gpx2kml.py "%s" >> "%s"' 
+    % ( gps_file, kml_file )
+  )
+
+
   
 
   
